dd197643810ea9a23886da15916329a0bf4f280d
Matt Hoffman <mwh30@cam.ac.uk>
2014-07-09 14:45:23 +0100

    Add BO with sampling.

--- demos/simple.py
+++ demos/simple.py
@@ -11,34 +11,38 @@ def run_model(model, policy, T):
     xmin = model.bounds[0,0]
     xmax = model.bounds[0,1]
     X = np.linspace(xmin, xmax, 200)[:, None]
+
     x = (xmax-xmin) / 2 + xmin
+    y = model(x)
+    policy.add_data(x, y)
 
     pl.figure(1)
     pl.show()
 
     for i in xrange(T):
-        y = model(x)
-        policy.add_data(x, y)
         x = policy.get_next()
 
-        pygp.plotting.gpplot(policy._gp, xmin=xmin, xmax=xmax, mean=False, draw=False)
-
-        pl.plot(X, model.get_f(X), lw=2, color='c')
-        pl.plot(X, policy._index(X), lw=2)
-        pl.axvline(x, color='r')
+        pl.clf()
+        pl.plot(X, model.get_f(X), 'k--', lw=2, zorder=1)
+        pl.plot(X, policy._index(X), lw=2, zorder=2)
+        pl.scatter(policy._gp._X, policy._gp._y, color='m', zorder=2)
+        pl.axvline(x, color='r', zorder=3)
         pl.axis('tight')
         pl.axis(ymin=-3.4, ymax=3.4, xmin=xmin, xmax=xmax)
         pl.draw()
 
+        y = model(x)
+        policy.add_data(x, y)
+
 
 if __name__ == '__main__':
-    sn = 0.2
-    sf = 1.25
-    ell = 0.05
-    bounds = [0.5, 2.5]
+    # sn = 0.2
+    # sf = 9.56
+    # ell = 0.42
+    # bounds = [0.5, 2.5]
     T = 100
 
-    model = pybo.models.Gramacy(sn)
-    policy = pybo.policies.GPPolicy(model.bounds, sn, sf, ell, 'gpucb')
+    model = pybo.models.Sinusoidal(0.2)
+    policy = pybo.policies.GPPolicy(model.bounds, acq='gpucb')
 
     run_model(model, policy, T)
--- pybo/policies/gppolicy.py
+++ pybo/policies/gppolicy.py
@@ -12,13 +12,13 @@ from __future__ import print_function
 import numpy as np
 import scipy.stats as ss
 
+# not "exactly" local, but...
+import pygp
+import pygp.extra.fourier as fourier
+
 # local imports
 from ._base import Policy
 from ._direct import solve_direct
-
-# not "exactly" local, but...
-from pygp import BasicGP
-from pygp.extra.fourier import FourierSample
 
 # exported symbols
 __all__ = ['GPPolicy']
@@ -79,30 +79,49 @@ def gpucb(gp, delta=0.1, xi=0.2):
 
 @_register
 def thompson(gp, nfeatures=250):
-    return FourierSample(gp, nfeatures)
+    return fourier.FourierSample(gp, nfeatures)
 
 
 #===============================================================================
 # define the meta policy.
 
 class GPPolicy(Policy):
-    def __init__(self, bounds, sn, sf, ell, acq='gpucb', **extra):
+    def __init__(self,
+                 bounds, ndim=None, acq='gpucb', kernel='Matern3',
+                 **extra):
+
         if acq not in ACQUISITION_FUNCTIONS:
             raise RuntimeError('unknown acquisition function')
 
         self._bounds = np.array(bounds, dtype=float, ndmin=2)
-        self._gp = BasicGP(sn, sf, np.array(ell, ndmin=1))
         self._extra = extra
+
+        sn = 0.5
+        sf = 1.0
+        ell = (bounds[:,1] - bounds[:,0]) / 10
+        self._gp = pygp.BasicGP(sn, sf, ell, kernel=kernel)
+
+        self._priors = dict(
+            sn =pygp.priors.Uniform(0.01,  1.0),
+            sf =pygp.priors.Uniform(0.01, 10.0),
+            ell=pygp.priors.Uniform(0.01,  1.0))
 
         # _acq generates _index every time we add data. technically we don't
         # really need to save the index, but it's useful for
         # debugging/visualization purposes.
         self._acq = ACQUISITION_FUNCTIONS[acq]
-        self._index = self._acq(self._gp, **self._extra)
+        self._index = None
 
     def add_data(self, x, y):
         self._gp.add_data(x, y)
-        self._index = self._acq(self._gp, **self._extra)
+
+        n = 100
+        m =   1
+        hypers = pygp.sample(self._gp, self._priors, n)
+        acqs = [self._acq(self._gp.copy(h), **self._extra) for h in hypers[-m:]]
+
+        self._index = lambda x: sum(a(x) for a in acqs) / m
+        # self._index = self._acq(self._gp, **self._extra)
 
     def get_next(self):
         if self._gp.ndata == 0:
