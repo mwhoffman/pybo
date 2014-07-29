0c458fa35a1bcca4d978b802f84ff288c76e921b
Bobak Shahriari <bshahr@cs.ubc.ca>
2014-07-29 16:44:53 -0700

    Added 2D test for EI gradient

--- tests/test_gradients.py
+++ tests/test_gradients.py
@@ -36,5 +36,30 @@ def test_ei_gradients():
     nt.assert_allclose(dei, dei_, rtol=1e-6, atol=1e-6)
 
 
-if __name__ == '__main__':
-    test_ei_gradients()
+def test_ei_gradients_2d():
+    rng = np.random.RandomState(0)
+    model = pybo.models.Branin()
+    policy = pybo.policies.GPPolicy(model.bounds, policy='ei')
+
+    xmin = model.bounds[:, 0]
+    xmax = model.bounds[:, 1]
+    x = (xmax - xmin) / 2 + xmin
+    for t in xrange(5):
+        y = model(x)
+        policy.add_data(x, y)
+        x = policy.get_next()
+
+    # get some test points
+    xtest = rng.rand(20, 2) * (xmax - xmin) + xmin
+
+    # compute gradients analytically
+    _, dei = policy._index(xtest, grad=True)
+
+    # define functions to feed into approx_fprime. These take a single point so
+    # we have to use the None-index to "vectorize" it.
+    fei = lambda x: policy._index(x[None], grad=False)
+    # numerical approximation of gradients
+    dei_ = np.array([spop.approx_fprime(xk, fei, 1e-8) for xk in xtest])
+
+    # make sure everything is close to within tolerance
+    nt.assert_allclose(dei, dei_, rtol=1e-6, atol=1e-6)
