bf7777e6160d8bf7db3cbeade1e6b5c0b07f75f9
Bobak Shahriari <bshahr@cs.ubc.ca>
2014-07-29 18:35:54 -0700

    Added PI and UCB with gradients. Tests almost there...

--- pybo/policies/gpacquisition.py
+++ pybo/policies/gpacquisition.py
@@ -14,7 +14,7 @@ import functools
 import operator
 
 # exported symbols
-__all__ = ['ei']
+__all__ = ['ei', 'pi', 'ucb']
 
 
 def _integrate(models, index):
@@ -67,29 +67,48 @@ def ei(models, fbest, xi=0.0):
     return _integrate(models, index)
 
 
-# FIXME: just comment out the other acquisition functions for now. NOTE that
-# they've also been removed from __all__.
+def pi(models, fbest, xi=0.05):
+    target = fbest + xi
+
+    def index(model, grad, X):
+        posterior = model.posterior(X, grad=grad)
+        mu, s2 = posterior[:2]
+        s = np.sqrt(s2)
+        d = mu - target
+        z = d / s
+        cdfz = ss.norm.cdf(z)
+
+        if grad:
+            # get the derivative of pi. The mu/s2/etc. components are vectors
+            # collecting n scalar points, whereas dmu and ds2 are (n,d)-arrays.
+            # The indexing tricks just interpret the "scalar" quantities as
+            # (n,1)-arrays so that we can use numpy's broadcasting rules.
+            dmu, ds2 = posterior[2:]
+            dz = dmu / s[:, None] - 0.5 * ds2 * z[:, None] / s2[:, None]
+            pdfz = ss.norm.pdf(z)
+
+            return cdfz, dz * pdfz
+        else:
+            return cdfz
+
+    return _integrate(models, index)
 
 
-# def pi(gp, xi=0.05):
-#     fmax = gp.get_max()[1] if (gp.ndata > 0) else 0
-#     def index(X):
-#         mu, s2 = gp.posterior(X)
-#         mu -= fmax + xi
-#         mu /= np.sqrt(s2, out=s2)
-#         return mu
-#     return index
+def ucb(models, delta=0.1, xi=0.2):
+    d = models._kernel.ndim
+    a = xi * 2 * np.log(np.pi**2 / 3 / delta)
+    b = xi * (4 + d)
 
-
-# def ucb(gp, delta=0.1, xi=0.2):
-#     d = gp._kernel.ndim
-#     a = xi*2*np.log(np.pi**2 / 3 / delta)
-#     b = xi*(4+d)
-#     def index(X):
-#         mu, s2 = gp.posterior(X)
-#         beta = a + b * np.log(gp.ndata+1)
-#         return mu + np.sqrt(beta*s2)
-#     return index
+    def index(model, grad, X):
+        posterior = model.posterior(X, grad=grad)
+        mu, s2 = posterior[:2]
+        beta = a + b * np.log(model.ndata + 1)
+        if grad:
+            dmu, ds2 = posterior[2:]
+            return mu + np.sqrt(beta * s2), dmu + np.sqrt(beta * ds2)
+        else:
+            return mu + np.sqrt(beta * s2)
+    return _integrate(models, index)
 
 
 # def thompson(gp, nfeatures=250):
