16067698dc2ca0f9018f97be7a56c7646d7f2828
Matt Hoffman <mwh30@cam.ac.uk>
2014-08-18 18:59:10 +0100

    Get rid of cython DIRECT implementation.

--- pybo/globalopt/__init__.py
+++ pybo/globalopt/__init__.py
@@ -3,12 +3,9 @@ Objects which global optimization solvers.
 """
 
 # pylint: disable=wildcard-import
-from .direct import *
 from .lbfgs import *
 
-from . import direct
 from . import lbfgs
 
 __all__ = []
-__all__ += direct.__all__
 __all__ += lbfgs.__all__
