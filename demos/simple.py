56c23309d76f15bf548f3adbf3b80efe6ef2d254
Matt Hoffman <mwh30@cam.ac.uk>
2014-09-08 12:48:27 +0100

    Clean up the demo.

--- demos/simple.py
+++ demos/simple.py
@@ -1,13 +1,21 @@
+"""
+Simple demo which performs Bayesian optimization on a single objective function
+and visualizes the search process while doing so.
+"""
+
 import numpy as np
 import matplotlib.pyplot as pl
 
 import pygp
-import pygp.plotting
+import pygp.plotting as pp
 import pybo.models
 import pybo.policies
 
 
 def callback(info, x, f, model, bounds, index):
+    """
+    Plot the current posterior and the index.
+    """
     xmin = bounds[0, 0]
     xmax = bounds[0, 1]
 
@@ -17,38 +25,38 @@ def callback(info, x, f, model, bounds, index):
     ymin -= 0.2 * (ymax - ymin)
     ymax += 0.4 * (ymax - ymin)
 
-
-    pygp.plotting.plot(model, xmin=xmin, xmax=xmax,
-                       ymin=ymin, ymax=ymax,
-                       subplot=211, draw=False)
+    pl.subplot(211)
+    pl.cla()
+    pp.plot_posterior(model, xmin=xmin, xmax=xmax)
+    pl.axis((xmin, xmax, ymin, ymax))
 
     pl.plot(X, F, 'k--', lw=2)
     pl.axvline(x, color='r')
     pl.axvline(info[-1]['xbest'], color='g')
-    pl.ylabel('posterior')
     pl.gca().set_xticklabels([])
     pl.gca().set_yticklabels([])
+    pl.ylabel('posterior')
 
     pl.subplot(212)
     pl.cla()
     pl.plot(X, index(X), lw=2)
     pl.axvline(x, color='r')
     pl.axis(xmin=xmin, xmax=xmax)
-    pl.ylabel('acquisition')
     pl.gca().set_xticklabels([])
     pl.gca().set_yticklabels([])
+    pl.ylabel('acquisition')
     pl.draw()
 
 
 if __name__ == '__main__':
     T = 100
     sigma = 0.05
-    model = pygp.BasicGP(sigma, 1.0, 0.1, kernel='matern3')
-    f = pybo.models.GPModel([3, 5], model)
+    gp = pygp.BasicGP(sigma, 1.0, 0.1, kernel='matern3')
+    f = pybo.models.GPModel([3, 5], gp)
 
     pybo.policies.solve_bayesopt(f,
                                  f.bounds,
                                  sigma,
                                  policy='ucb',
-                                 kernel=model._kernel.copy(),
+                                 kernel=gp._kernel.copy(),
                                  callback=callback)
