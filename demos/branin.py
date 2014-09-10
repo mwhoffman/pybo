718effb8176763c8334455b1a9cb3bec4149479a
Matt Hoffman <mwh30@cam.ac.uk>
2014-09-10 15:46:26 +0100

    Update the 2d demo to show more.

--- demos/branin.py
+++ demos/branin.py
@@ -23,6 +23,9 @@ def callback(info, x, f, model, bounds, index):
     F = f.get_f(Z).reshape(Z0.shape)
     I = index(Z).reshape(Z0.shape)
 
+    M, _ = model.posterior(Z)
+    M = M.reshape(Z0.shape)
+
     X = info['x']
     xbest = info[-1]['xbest']
 
@@ -34,11 +37,19 @@ def callback(info, x, f, model, bounds, index):
     pl.axis(f.bounds.flatten())
     pl.gca().set_xticklabels([])
     pl.gca().set_yticklabels([])
-    pl.ylabel('true function')
+    pl.title('true function')
 
     pl.scatter(x[0], x[1], marker='o', s=20, color='k', zorder=2)
     pl.scatter(X[:, 0], X[:, 1], facecolors='none', marker='o', s=20, lw=1,
                color='k', zorder=2)
+    pl.scatter(xbest[0], xbest[1], marker='+', s=50, lw=2, color='k', zorder=2)
+
+    pl.subplot(222)
+    pl.contour(Z0, Z1, M, cmap='coolwarm')
+    pl.axis(f.bounds.flatten())
+    pl.gca().set_xticklabels([])
+    pl.gca().set_yticklabels([])
+    pl.title('posterior mean')
     pl.scatter(xbest[0], xbest[1], marker='+', s=50, lw=2, color='k', zorder=2)
 
     pl.subplot(223)
@@ -46,12 +57,12 @@ def callback(info, x, f, model, bounds, index):
     pl.axis(f.bounds.flatten())
     pl.gca().set_xticklabels([])
     pl.gca().set_yticklabels([])
-    pl.ylabel('acquisition')
+    pl.title('acquisition')
 
     pl.scatter(x[0], x[1], marker='o', s=20, color='k', zorder=2)
 
     if not all(np.isnan(info['fbest'])):
-        pl.subplot(122)
+        pl.subplot(224)
         pl.semilogy(f.fmax - info['fbest'], lw=2)
         pl.axis('tight')
         pl.title('regret of recommendation')
@@ -59,15 +70,29 @@ def callback(info, x, f, model, bounds, index):
     pl.draw()
     pl.show(block=False)
 
+    pl.figure(2)
+    pp.plot_samples(model)
+
+    pl.draw()
+    pl.show(block=False)
+
 
 if __name__ == '__main__':
     T = 100
-    sigma = 1e-1
-    gp = pygp.BasicGP(sigma, 1.0, 0.1, ndim=2, kernel='matern3')
-    f = pybo.functions.Branin(sigma)
+    sigma = 1e-10
+    gp = pygp.BasicGP(sigma, 1.0, [0.1, 0.1], 0, kernel='matern3')
+    f = pybo.functions.Branin(0.0)
+
+    prior = {
+        'sn': None,
+        'sf': pygp.priors.Uniform(0.01, 5.0),
+        'ell': pygp.priors.Uniform(np.full(2, 0.01), np.full(2, 5.0)),
+        'mu': pygp.priors.Uniform(-10, 10)}
 
     info = pybo.solve_bayesopt(f,
                                f.bounds,
+                               gp=gp,
+                               prior=prior,
                                policy='ei',
                                inference='smc',
                                callback=callback)
