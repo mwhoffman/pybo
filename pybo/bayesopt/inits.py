8998d0f617791f95b1ed6b4a1fffa0f71752b801
Matt Hoffman <mwh30@cam.ac.uk>
2014-11-13 19:16:02 +0000

    Update docs/params for initialization methods.

--- pybo/bayesopt/inits.py
+++ pybo/bayesopt/inits.py
@@ -18,22 +18,37 @@ __all__ = ['init_middle', 'init_uniform', 'init_latin', 'init_sobol']
 
 
 def init_middle(bounds):
+    """
+    Initialize using a single query in the middle of the space.
+    """
     return np.mean(bounds, axis=1)[None, :]
 
 
-def init_uniform(bounds, rng=None):
-    n = 3*len(bounds)
+def init_uniform(bounds, n=None, rng=None):
+    """
+    Initialize using `n` uniformly distributed query points. If `n` is `None`
+    then use 3D points where D is the dimensionality of the input space.
+    """
+    n = 3*len(bounds) if (n is None) else n
     X = ldsample.random(bounds, n, rng)
     return X
 
 
-def init_latin(bounds, rng=None):
-    n = 3*len(bounds)
+def init_latin(bounds, n=None, rng=None):
+    """
+    Initialize using a Latin hypercube design of size `n`. If `n` is `None`
+    then use 3D points where D is the dimensionality of the input space.
+    """
+    n = 3*len(bounds) if (n is None) else n
     X = ldsample.latin(bounds, n, rng)
     return X
 
 
-def init_sobol(bounds, rng=None):
-    n = 3*len(bounds)
+def init_sobol(bounds, n=None, rng=None):
+    """
+    Initialize using a Sobol sequence of length `n`. If `n` is `None` then use
+    3D points where D is the dimensionality of the input space.
+    """
+    n = 3*len(bounds) if (n is None) else n
     X = ldsample.sobol(bounds, n, rng)
     return X
