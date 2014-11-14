812d3cdace821a77fdcb2e0441ba5fa2650bf5fd
Matt Hoffman <mwh30@cam.ac.uk>
2014-11-14 12:10:36 +0000

    Update params decorator with basic error checking.

--- pybo/bayesopt/utils.py
+++ pybo/bayesopt/utils.py
@@ -7,6 +7,9 @@ from __future__ import division
 from __future__ import absolute_import
 from __future__ import print_function
 
+# global imports
+import inspect
+
 # exported symbols
 __all__ = ['params']
 
@@ -17,7 +20,25 @@ def params(*args):
     modified by the user.
     """
     def decorator(f):
-        """Internal decorator to perform the annotation."""
-        f._params = set(args)
+        """
+        Internal decorator to perform the annotation.
+        """
+        spec = inspect.getargspec(f)
+        params_valid = set(spec.args[::-1][:len(spec.defaults)])
+        params = set(args)
+
+        # make sure we're exposing valid parameters which are actually kwargs
+        # in the decorated function.
+        if not params.issubset(params_valid):
+            raise ValueError('exposed parameters are not valid kwargs: %r'
+                             % list(params - params_valid))
+
+        # make sure we're not trying to expose rng.
+        if 'rng' in params:
+            raise ValueError("'rng' is a special parameter that "
+                             "shouldn't be exposed")
+
+        f._params = args
         return f
+
     return decorator
