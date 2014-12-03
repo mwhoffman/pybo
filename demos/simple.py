f4b3031d9bbe989ef53a2fa49ed3061de891926d
Bobak Shahriari <bshahr@cs.ubc.ca>
2014-12-02 17:22:41 -0800

    Polished demos/simple.py.

--- demos/simple.py
+++ demos/simple.py
@@ -1,6 +1,7 @@
 """
 Simple demo which performs Bayesian optimization on a single objective function
-and visualizes the search process while doing so.
+sampled from a known GP prior. This script also provides an example
+visualization of the search process via the callback argument.
 """
 
 import numpy as np
@@ -13,65 +14,77 @@ import pybo
 
 def callback(model, bounds, info, x, index, ftrue):
     """
-    Plot the current posterior and the index.
+    Plot the current posterior, the index, and the value of the current
+    recommendation.
     """
     xmin = bounds[0, 0]
     xmax = bounds[0, 1]
 
-    X = np.linspace(xmin, xmax, 500)[:, None]
-    F = ftrue(X)
-    ymin, ymax = F.min(), F.max()
+    xx = np.linspace(xmin, xmax, 500)[:, None]
+    ff = ftrue(xx)
+    ymin, ymax = ff.min(), ff.max()
     ymin -= 0.2 * (ymax - ymin)
     ymax += 0.2 * (ymax - ymin)
 
-    pl.figure(1)
-    pl.clf()
+    # common plotting kwargs
+    kwplot = {'lw': 2,
+              'alpha': 0.5}
+
+    fig = pl.figure(1)
+    fig.clf()
+
+    # plot the surrogate and data
     pl.subplot(221)
     pp.plot_posterior(model, xmin=xmin, xmax=xmax)
-    pl.plot(X, F, 'k--', lw=2)
-    pl.axvline(x, color='r')
-    pl.axvline(info[-1]['xbest'], color='g')
+    pl.plot(xx, ff, 'k--', **kwplot)
+    pl.axvline(x, color='r', **kwplot)
+    pl.axvline(info[-1]['xbest'], color='g', **kwplot)
     pl.axis((xmin, xmax, ymin, ymax))
-    pl.gca().set_xticklabels([])
-    pl.gca().set_yticklabels([])
     pl.ylabel('posterior')
 
+    # plot the acquisition function
     pl.subplot(223)
-    pl.plot(X, index(X), lw=2)
-    pl.axvline(x, color='r')
-    pl.axis(xmin=xmin, xmax=xmax)
-    pl.gca().set_xticklabels([])
-    pl.gca().set_yticklabels([])
+    pl.fill_between(xx.ravel(), 0., index(xx), color='r', alpha=0.1)
+    pl.axis('tight')
+    pl.axvline(x, color='r', **kwplot)
     pl.xlabel('input')
     pl.ylabel('acquisition')
 
-    pl.subplot(122)
-    pl.plot(ftrue(info['xbest']), lw=2)
-    pl.axis('tight')
+    # plot the value at the current recommendation
+    pl.subplot(222)
+    pl.plot(ftrue(info['xbest']), 'g', **kwplot)
+    pl.axis((0, len(info['xbest']), ymin, ymax))
     pl.xlabel('iterations')
     pl.ylabel('value of recommendation')
-    pl.gca().set_xticklabels([])
-    pl.gca().set_yticklabels([])
+
+    for ax in fig.axes:
+        ax.set_xticklabels([])
+        ax.set_yticklabels([])
+
     pl.draw()
     pl.show(block=False)
 
 
 if __name__ == '__main__':
-    sigma = 1e-6
-    mean = 0.0
-    rng = 0
+    noise = 1e-6    # used as likelihood std dev as well as observation noise
+    mean = 0.0      # GP prior mean (constant)
+    rng = 0         # random seed
 
-    likelihood = pygp.likelihoods.Gaussian(sigma)
-    kernel = pygp.kernels.Periodic(1, 1, 0.5) + \
-             pygp.kernels.SE(1, 1)
-
+    # define GP prior from which the objective function will be sampled
+    likelihood = pygp.likelihoods.Gaussian(noise)
+    kernel = (pygp.kernels.Periodic(1, 1, 0.5) +
+              pygp.kernels.SE(1, 1))
     gp = pygp.inference.ExactGP(likelihood, kernel, mean)
-    f = pybo.functions.GPModel([3, 5], gp, rng=rng)
-    T = 30 * f.bounds.shape[0]
 
-    info = pybo.solve_bayesopt(f,
-                               f.bounds,
-                               T=T,
+    # sample objective function from GP prior
+    objective = pybo.functions.GPModel([3, 5], gp, rng=rng)
+
+    dim = objective.bounds.shape[0]     # dimension of problem
+    niter = 30 * dim                    # number of iterations (horizon)
+
+    info = pybo.solve_bayesopt(objective,
+                               objective.bounds,
+                               T=niter,
                                policy='ei',
                                init='latin',
                                recommender='incumbent',
@@ -79,6 +92,4 @@ if __name__ == '__main__':
                                rng=rng,
                                callback=callback)
 
-    # this makes sure that if we run the demo from the command line that it
-    # stops on the final plot before closing.
     pl.show()
