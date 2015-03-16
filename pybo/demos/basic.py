76ffa6372170b571286d1165044ad2f5a62587b9
Matt Hoffman <mwh30@cam.ac.uk>
2015-03-16 19:27:25 +0000

    Update the solvers.

--- pybo/demos/basic.py
+++ pybo/demos/basic.py
@@ -6,35 +6,36 @@ import reggie
 
 from pybo.bayesopt import inits
 from pybo.bayesopt import policies
+from pybo.bayesopt import solvers
 
 
 if __name__ == '__main__':
     f = benchfunk.Gramacy(0.2)
-    X = inits.init_latin(f.bounds, 100)
+    X = inits.init_latin(f.bounds, 10)
     Y = f.get(X)
+
+    xmin, xmax = f.bounds[0]
+    Z = np.linspace(xmin, xmax, 500)[:, None]
 
     model = reggie.BasicGP(0.2, 1.9, 0.1, -1)
     model.add_data(X, Y)
-    index_pi = policies.PI(model)
-
-    Z = np.linspace(f.bounds[0][0], f.bounds[0][1], 500)[:, None]
-
-    EI = policies.EI(model)(Z)
-    EI += np.min(EI)
-    EI /= np.max(EI)
-
-    PI = policies.PI(model)(Z)
-    PI += np.min(PI)
-    PI /= np.max(PI)
 
     pl.figure(1)
     pl.clf()
     pl.subplot(211)
-    reggie.plotting.plot_posterior(model)
+    reggie.plotting.plot_posterior(model, xmin=xmin, xmax=xmax)
+    pl.legend(loc=0)
     xmin, xmax, _, _ = pl.axis()
 
     pl.subplot(212)
-    pl.plot(Z, EI, label='ei')
-    pl.plot(Z, PI, label='pi')
+    for policy in [policies.EI, policies.PI]:
+        index = policy(model)
+        xstar, _ = solvers.solve_lbfgs(index, f.bounds)
+        vals = index(Z)
+        vals += np.min(vals)
+        vals /= np.max(vals)
+        lines = pl.plot(Z, vals, label=policy.__name__.lower())
+        pl.axvline(xstar, ls='--', color=lines[0].get_color())
+
     pl.legend(loc=0)
     pl.axis(xmin=xmin, xmax=xmax)
