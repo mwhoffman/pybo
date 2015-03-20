185d55b5e39bc2565aa42eb6a7cf0185c9b3b256
Matt Hoffman <mwh30@cam.ac.uk>
2015-03-20 13:37:06 +0000

    Update demo.

--- pybo/demos/basic.py
+++ pybo/demos/basic.py
@@ -1,51 +1,50 @@
 import numpy as np
-import matplotlib.pyplot as pl
-
 import benchfunk
 import reggie
+import mwhutils.plotting as mp
 
 from pybo.bayesopt import inits
 from pybo.bayesopt import policies
 from pybo.bayesopt import solvers
-from pybo.bayesopt import recommenders as recs
+from pybo.bayesopt import recommenders
 
 
 if __name__ == '__main__':
+    # grab a test function and points at which to plot things
     f = benchfunk.Gramacy(0.2)
+    x = np.linspace(f.bounds[0, 0], f.bounds[0, 1], 500)
+
+    # get initial data
     X = inits.init_latin(f.bounds, 20)
     Y = f.get(X)
 
-    xmin, xmax = f.bounds[0]
-    Z = np.linspace(xmin, xmax, 500)[:, None]
-
+    # initialize the model
     model = reggie.BasicGP(0.2, 1.9, 0.1, -1)
     model.add_data(X, Y)
 
     while True:
-        xbest = recs.best_incumbent(model, f.bounds)
+        xbest = recommenders.best_incumbent(model, f.bounds)
         index = policies.EI(model)
         xnext, _ = solvers.solve_lbfgs(index, f.bounds)
-        T = Z.ravel()
-        I = index(Z)
 
-        pl.figure(1)
-        pl.clf()
-        color1 = next(pl.gca()._get_lines.color_cycle)
-        color2 = next(pl.gca()._get_lines.color_cycle)
+        mu, s2 = model.get_posterior(x[:, None])
+        lo = mu - 2*np.sqrt(s2)
+        hi = mu + 2*np.sqrt(s2)
 
-        pl.clf()
-        pl.subplot(211)
-        reggie.plotting.plot_posterior(model, xmin=xmin, xmax=xmax, draw=False)
-        pl.axvline(xbest, ls='--', color=color1, label='recommendation')
-        pl.axis(xmin=xmin, xmax=xmax)
-        pl.legend(loc=0)
+        # plot the observed data
+        fig = mp.figure(1, 3)
+        fig[0].scatter(model.data[0].ravel(), model.data[1])
 
-        pl.subplot(212)
-        pl.plot(T, I, color=color2, label='acquisition')
-        pl.fill_between(T, np.zeros_like(I), I, color=color2, alpha=0.1)
-        pl.axvline(xnext, ls='--', color=color2, label='xnext')
-        pl.axis(xmin=xmin, xmax=xmax)
-        pl.legend(loc=0)
-        pl.draw()
+        # plot the posterior
+        fig[1].plot_banded(x, mu, lo, hi)
+        fig[1].plot(x, f.get_f(x[:, None]), ls='--', color='k')
+        fig[1].vline(xbest)
+
+        # plot the acquisition function
+        fig[2].plot_banded(x, index(x[:, None]))
+        fig[2].vline(xnext)
+
+        # draw
+        fig.draw()
 
         model.add_data(xnext, f(xnext))
