Fix scaling factor for PES.

pybo/demos/basic.py
@@ -21,12 +21,12 @@ if __name__ == '__main__':
     Y = np.array([f(x_) for x_ in X])
 
     # initialize the model
-    model = reggie.BasicGP(0.1, 1.9, 0.5, 0)
+    model = reggie.BasicGP(0.1, 1.9, 0.1, 0)
     model.add_data(X, Y)
 
     while True:
         xbest = recommenders.best_latent(model, bounds)
-        index = policies.EI(model, bounds)
+        index = policies.PES(model, bounds)
         xnext, _ = solvers.solve_direct(index, bounds)
 
         mu, s2 = model.get_posterior(x[:, None])
@@ -39,7 +39,7 @@ if __name__ == '__main__':
 
         # plot the posterior
         fig[1].plot_banded(x, mu, lo, hi)
-        fig[1].plot(x, f.get(x), ls='--', color='k')
+        fig[1].plot(x, f.get_f(x[:, None]), ls='--', color='k')
         fig[1].vline(xbest)
         fig[1].vline(f.xopt)
 

pybo/policies/pes.py
@@ -104,7 +104,7 @@ def get_predictions(gp, xstar, Xtest):
     s = s2 + s2star - 2*rho
 
     while any(s < 1e-10):
-        rho[s < 1e-10] *= 1 - 1e-4
+        rho[s < 1e-10] *= 1 - 1e-10
         s = s2 + s2star - 2*rho
 
     a = (mustar - mu) / np.sqrt(s)