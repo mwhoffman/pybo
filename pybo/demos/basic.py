dfe386afdafca7ffa819527f6b85f59536596873
Bobak Shahriari <bshahr@cs.ubc.ca>
2015-06-24 10:59:21 -0700

    Removes mwhutils dependence for plotting

--- pybo/demos/basic.py
+++ pybo/demos/basic.py
@@ -1,70 +1,91 @@
+import matplotlib as mpl
+import matplotlib.pyplot as plt
 import numpy as np
+
 import benchfunk
 import reggie
-import mwhutils.plotting as mp
-
 from pybo import inits
 from pybo import policies
 from pybo import solvers
 from pybo import recommenders
 
 
+# -- makes plots prettier. should eventually be moved out of here.
+mpl.rc('lines', lw=2)
+mpl.rc('legend', scatterpoints=1)
+mpl.rc('savefig', bbox='tight')
+
 if __name__ == '__main__':
-    # grab a test function and points at which to plot things
-    f = benchfunk.Gramacy(0.01)
+    # -- define noisy test function and search bounds
+    sn2 = 0.1 ** 2                  # Gaussian noise variance
+    f = benchfunk.Gramacy(sn2)
     bounds = f.bounds
 
-    # get initial data and some test points.
+    # -- get initial data and some test points.
     X = inits.init_latin(bounds, 3)
     Y = np.array([f(x_) for x_ in X])
     x = np.linspace(bounds[0][0], bounds[0][1], 500)
 
-    # initialize the model
-    model = reggie.make_gp(0.01, 1.9, 0.1, 0)
+    # -- initialize the model
+    rho = 2.0                       # initial kernel amplitude
+    ell = 0.1                       # initial kernel length-scale
+    mu = 0.0                        # constant prior mean
+    model = reggie.make_gp(sn2, rho, ell, mu)
     model.add_data(X, Y)
 
-    # set a prior on the parameters
+    # -- set priors on the hyperparameters
     model.params['like.sn2'].set_prior('uniform', 0.005, 0.015)
     model.params['kern.rho'].set_prior('lognormal', 0, 100)
     model.params['kern.ell'].set_prior('lognormal', 0, 10)
     model.params['mean.bias'].set_prior('normal', 0, 20)
 
-    # make a model which samples parameters
-    model = reggie.MCMC(model, n=20, rng=None)
+    # -- initialize a model with MCMC hyperparameter sampling
+    model = reggie.MCMC(model, n=10)
 
-    # create a new figure
-    fig = mp.figure(rows=2, figsize=(6, 8))
+    # -- create a new figure
+    fig, axs = plt.subplots(2, 2, figsize=(12, 8), sharex='col', sharey='row')
+    fig.show()
+    fbest = list()
 
     while True:
-        # get the index so we can both solve it and plot it
+        # -- get index to solve it and plot it
         index = policies.EI(model, bounds, 0.1)
 
-        # get the recommendation and the next query
+        # -- get the recommendation and the next query
         xbest = recommenders.best_latent(model, bounds)
         xnext, _ = solvers.solve_lbfgs(index, bounds)
 
-        # evaluate the posterior and the acquisition function
+        # -- evaluate the posterior and the acquisition function
         mu, s2 = model.predict(x[:, None])
         alpha = index(x[:, None])
 
-        # clear the figure
-        fig.clear()
-        fig.hold()
+        # -- plot the posterior and data
+        axs[0,0].clear()
+        axs[0,0].plot(x, mu, label='posterior')
+        axs[0,0].fill_between(x,
+                               mu - np.sqrt(s2),
+                               mu + np.sqrt(s2),
+                               alpha=0.1,
+                               label='uncertainty')
+        axs[0,0].plot(x, f.get_f(x[:, None]), label='true function')
+        axs[0,0].vlines(xbest, *axs[0, 0].get_ylim(), label='recommendation')
+        axs[0,0].scatter(model.data[0].ravel(), model.data[1], label='data')
 
-        # plot the posterior
-        fig[0].plot(x, mu, 2*np.sqrt(s2), label='posterior')
-        fig[0].plot(x, f.get_f(x[:, None]), label='true function')
-        fig[0].scatter(model.data[0].ravel(), model.data[1], label='data')
-        fig[0].vline(xbest, label='recommendation')
-        fig[0].set_lim(ymin=-7, ymax=2)
+        # -- plot the acquisition function
+        axs[1,0].clear()
+        axs[1,0].plot(x, alpha, label='acquisition')
+        axs[1,0].vlines(xnext, *axs[1, 0].get_ylim(), label='next query')
+        axs[1,0].set_xlim(*bounds)
 
-        # plot the acquisition function
-        fig[1].plot(x, alpha, 0, add=False, label='acquisition')
-        fig[1].vline(xnext, label='next query')
-        fig[1].remove_ticks(yticks=True)
+        # -- plot the latent function at recomended points
+        axs[0,1].clear()
+        fbest += [f.get_f(xbest)]
+        axs[0,1].plot(fbest)
+        axs[0,1].set_ylim([-7, 2])
 
         # draw
-        fig.draw()
+        [ax.legend(loc=0) for ax in axs.flatten()]
+        fig.canvas.draw()
 
         # add the next evaluation
         model.add_data(xnext, f(xnext))
