cb6e67427d98a97ede74218c5976d69665c97956
Matt Hoffman <mwh30@cam.ac.uk>
2015-08-09 01:56:17 +0100

    Update OPES.

--- pybo/policies/__init__.py
+++ pybo/policies/__init__.py
@@ -4,14 +4,8 @@ Acquisition functions.
 
 # pylint: disable=wildcard-import
 from .simple import *
-from .pes import *
-from .opes import *
 
 from . import simple
-from . import pes
-from . import opes
 
 __all__ = []
 __all__ += simple.__all__
-__all__ += pes.__all__
-__all__ += opes.__all__
--- pybo/policies/simple.py
+++ pybo/policies/simple.py
@@ -8,8 +8,11 @@ from __future__ import absolute_import
 from __future__ import print_function
 
 import numpy as np
+import mwhutils.random as random
 
-__all__ = ['EI', 'PI', 'UCB', 'Thompson']
+from ..solvers import solve_lbfgs
+
+__all__ = ['EI', 'PI', 'UCB', 'Thompson', 'OPES']
 
 
 def EI(model, _, xi=0.0):
@@ -66,5 +69,34 @@ def UCB(model, _, delta=0.1, xi=0.2):
                     dmu + 0.5 * np.sqrt(beta / s2[:, None]) * ds2)
         else:
             return mu + np.sqrt(beta * s2)
+
+    return index
+
+
+def OPES(model, bounds, nopt=50, nfeat=200, rng=None):
+    rng = random.rstate(rng)
+
+    if hasattr(model, '__iter__'):
+        conditionals = [
+            m.condition_fstar(solve_lbfgs(m.sample_f(nfeat, rng).get,
+                                          bounds)[1])
+            for m in model]
+    else:
+        conditionals = [
+            model.condition_fstar(solve_lbfgs(model.sample_f(nfeat, rng).get,
+                                              bounds)[1])
+            for _ in xrange(nopt)]
+
+    def index(X, grad=False):
+        if not grad:
+            H = model.get_entropy(X)
+            H -= np.mean([_.get_entropy(X) for _ in conditionals], axis=0)
+            return H
+        else:
+            H, dH = model.get_entropy(X, grad=True)
+            parts = zip(*[_.get_entropy(X, True) for _ in conditionals])
+            H -= np.mean(parts[0], axis=0)
+            dH -= np.mean(parts[1], axis=0)
+            return H, dH
 
     return index
