16bd25914bb0622323e1abf9f171617d30a4d70f
Matt Hoffman <mwh30@cam.ac.uk>
2015-09-08 18:24:35 +0100

    Update the plotting code.

--- pybo/demos/animated.py
+++ pybo/demos/animated.py
@@ -30,6 +30,56 @@ plt.rc('axes', color_cycle=[
     (0.9686274509803922, 0.5058823529411764, 0.7490196078431373)])
 
 
+from matplotlib.axes import Axes
+from matplotlib.figure import Figure
+from matplotlib.projections import register_projection
+
+
+class CustomAxes(Axes):
+    name = 'custom_defaults'
+
+    def cla(self):
+        super(CustomAxes, self).cla()
+        self.spines['top'].set_visible(False)
+        self.spines['right'].set_visible(False)
+        self.xaxis.set_tick_params(direction='out', top=False)
+        self.yaxis.set_tick_params(direction='out', right=False)
+
+    def scatter(self, x, y, **kwargs):
+        kwargs_ = dict(s=40, c='k', linewidths=1.5, marker='o', 
+                       facecolors='none')
+        kwargs_.update(kwargs)
+        super(CustomAxes, self).scatter(x, y, **kwargs_)
+
+    def plot_banded(self, x, y=None, b1=None, **kwargs):
+        if y is None:
+            lines = self.plot(x, **kwargs)
+        else:
+            lines = self.plot(x, y, **kwargs)
+        x = lines[0].get_xdata()
+        y = lines[0].get_ydata()
+        c = lines[0].get_color()
+        a = lines[0].get_alpha()
+        a = 1.0 if (a is None) else a
+        if b1 is None:
+            self.fill_between(x, 0, y, color=c, alpha=0.2*a)
+        else:
+            self.fill_between(x, y-b1, y+b1, color=c, alpha=0.2*a)
+
+    def axvline(self, *args, **kwargs):
+        kwargs_ = dict(color='r', linestyle='--')
+        kwargs_.update(kwargs)
+        super(CustomAxes, self).axvline(*args, **kwargs_)
+
+    def axhline(self, *args, **kwargs):
+        kwargs_ = dict(color='r', linestyle='--')
+        kwargs_.update(kwargs)
+        super(CustomAxes, self).axhline(*args, **kwargs_)
+
+
+register_projection(CustomAxes)
+
+
 if __name__ == '__main__':
     # grab a test function and points at which to plot things
     f = Gramacy()
@@ -38,7 +88,6 @@ if __name__ == '__main__':
     # get initial data and some test points.
     X = inits.init_latin(bounds, 3)
     Y = np.array([f(x_) for x_ in X])
-    x = np.linspace(bounds[0][0], bounds[0][1], 500)
 
     # initialize the model
     model = make_gp(0.01, 1.9, 0.1, 0)
@@ -53,6 +102,7 @@ if __name__ == '__main__':
     # make a model which samples parameters
     model = MCMC(model, n=20, rng=None)
     fbest = list()
+    x = np.linspace(bounds[0][0], bounds[0][1], 500)
 
     # create a new figure
     fig = plt.figure(figsize=(10, 6))
@@ -68,14 +118,15 @@ if __name__ == '__main__':
 
         # evaluate the posterior and the acquisition function
         mu, s2 = model.predict(x[:, None])
-        s = np.sqrt(s2)
         alpha = index(x[:, None])
         fbest += [f.get_f(xbest)]
+        xdata = model.data[0].ravel()
+        ydata = model.data[1]
 
         fig.clear()
-        ax1 = plt.subplot2grid((2, 2), (0, 0))
-        ax2 = plt.subplot2grid((2, 2), (1, 0), sharex=ax1)
-        ax3 = plt.subplot2grid((2, 2), (0, 1), rowspan=2)
+        ax1 = plt.subplot2grid((2, 2), (0, 0), projection='custom_defaults')
+        ax2 = plt.subplot2grid((2, 2), (1, 0), projection='custom_defaults', sharex=ax1)
+        ax3 = plt.subplot2grid((2, 2), (0, 1), projection='custom_defaults', rowspan=2)
 
         plt.setp(ax1.get_xticklabels(), visible=False)
         plt.setp(ax2.get_yticklabels(), visible=False)
@@ -83,33 +134,24 @@ if __name__ == '__main__':
 
         # plot the posterior and data
         ax1.set_title('current model')
-        ax1.plot(x, mu, label='posterior')
-        ax1.fill_between(x, mu-2*s, mu+2*s, alpha=0.2,
-                         color=ax1.lines[-1].get_color())
-        ax1.scatter(model.data[0].ravel(), model.data[1], 40, lw=1.5, c='k',
-                    marker='o', facecolors='none', label='data')
-        ax1.axvline(xbest, c='r', ls='--', label='recomm.')
+        ax1.plot_banded(x, mu, 2*np.sqrt(s2), label='posterior')
+        ax1.scatter(xdata, ydata, label='data')
+        ax1.axvline(xbest, label='recomm.')
+        ax1.legend(loc=3, bbox_to_anchor=(1, 0))
 
         # plot the acquisition function
         ax2.set_title('current policy')
-        ax2.plot(x, alpha, label='acquisition')
-        ax2.fill_between(x, 0, alpha, alpha=0.2,
-                         color=ax2.lines[-1].get_color())
-        ax2.axvline(xnext, c='r', ls='--', label='next query')
+        ax2.plot_banded(x, alpha, label='acquisition')
+        ax2.axvline(xnext, label='next query')
         ax2.set_xlim(*bounds)
+        ax2.legend(loc=3, bbox_to_anchor=(1, 0))
 
         # plot the latent function at recomended points
         ax3.set_title('value of recommendation')
-        ax3.axhline(f.get_f(f.xopt), c='r', ls='--')
+        ax3.axhline(f.get_f(f.xopt))
         ax3.plot(fbest)
 
         # draw
-        for ax in fig.axes:
-            ax.spines['top'].set_visible(False)
-            ax.spines['right'].set_visible(False)
-            ax.xaxis.set_tick_params(direction='out', top=False)
-            ax.yaxis.set_tick_params(direction='out', right=False)
-            ax.legend(loc=3, bbox_to_anchor=(1, 0))
         fig.canvas.draw()
 
         # add the next evaluation
