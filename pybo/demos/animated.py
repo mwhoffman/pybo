1a08b8b3a39b8187139f489035889152835d7c90
Matt Hoffman <mwh30@cam.ac.uk>
2015-09-10 11:11:22 +0100

    Update the plotting demos.

--- pybo/demos/animated.py
+++ pybo/demos/animated.py
@@ -3,81 +3,16 @@ Animated demo showing progress of Bayesian optimization on a simple
 (but highly multimodal) one-dimensional function.
 """
 
-import matplotlib.pyplot as plt
 import numpy as np
 
 from reggie import make_gp, MCMC
 from benchfunk import Gramacy
+from ezplot import figure, show
 
 from pybo import inits
 from pybo import policies
 from pybo import solvers
 from pybo import recommenders
-
-
-# we shouldn't be doing this. but I'll leave it for now.
-plt.rc('lines', linewidth=2.0)
-plt.rc('legend', scatterpoints=3)
-plt.rc('figure', facecolor='white')
-plt.rc('axes', grid=True)
-plt.rc('grid', color='k', linestyle='-', alpha=0.2, linewidth=0.5)
-plt.rc('axes', color_cycle=[
-    (0.21568627450980393, 0.49411764705882355, 0.7215686274509804),
-    (0.30196078431372547, 0.6862745098039216, 0.2901960784313726),
-    (0.596078431372549, 0.3058823529411765, 0.6392156862745098),
-    (1.0, 0.4980392156862745, 0.0),
-    (0.6509803921568628, 0.33725490196078434, 0.1568627450980392),
-    (0.9686274509803922, 0.5058823529411764, 0.7490196078431373)])
-
-
-from matplotlib.axes import Axes
-from matplotlib.figure import Figure
-from matplotlib.projections import register_projection
-
-
-class CustomAxes(Axes):
-    name = 'custom_defaults'
-
-    def cla(self):
-        super(CustomAxes, self).cla()
-        self.spines['top'].set_visible(False)
-        self.spines['right'].set_visible(False)
-        self.xaxis.set_tick_params(direction='out', top=False)
-        self.yaxis.set_tick_params(direction='out', right=False)
-
-    def scatter(self, x, y, **kwargs):
-        kwargs_ = dict(s=40, c='k', linewidths=1.5, marker='o', 
-                       facecolors='none')
-        kwargs_.update(kwargs)
-        super(CustomAxes, self).scatter(x, y, **kwargs_)
-
-    def plot_banded(self, x, y=None, b1=None, **kwargs):
-        if y is None:
-            lines = self.plot(x, **kwargs)
-        else:
-            lines = self.plot(x, y, **kwargs)
-        x = lines[0].get_xdata()
-        y = lines[0].get_ydata()
-        c = lines[0].get_color()
-        a = lines[0].get_alpha()
-        a = 1.0 if (a is None) else a
-        if b1 is None:
-            self.fill_between(x, 0, y, color=c, alpha=0.2*a)
-        else:
-            self.fill_between(x, y-b1, y+b1, color=c, alpha=0.2*a)
-
-    def axvline(self, *args, **kwargs):
-        kwargs_ = dict(color='r', linestyle='--')
-        kwargs_.update(kwargs)
-        super(CustomAxes, self).axvline(*args, **kwargs_)
-
-    def axhline(self, *args, **kwargs):
-        kwargs_ = dict(color='r', linestyle='--')
-        kwargs_.update(kwargs)
-        super(CustomAxes, self).axhline(*args, **kwargs_)
-
-
-register_projection(CustomAxes)
 
 
 if __name__ == '__main__':
@@ -105,8 +40,7 @@ if __name__ == '__main__':
     x = np.linspace(bounds[0][0], bounds[0][1], 500)
 
     # create a new figure
-    fig = plt.figure(figsize=(10, 6))
-    fig.set_tight_layout(dict(w_pad=12))
+    fig = figure(figsize=(10, 6))
 
     while True:
         # get index to solve it and plot it
@@ -124,35 +58,33 @@ if __name__ == '__main__':
         ydata = model.data[1]
 
         fig.clear()
-        ax1 = plt.subplot2grid((2, 2), (0, 0), projection='custom_defaults')
-        ax2 = plt.subplot2grid((2, 2), (1, 0), projection='custom_defaults', sharex=ax1)
-        ax3 = plt.subplot2grid((2, 2), (0, 1), projection='custom_defaults', rowspan=2)
-
-        plt.setp(ax1.get_xticklabels(), visible=False)
-        plt.setp(ax2.get_yticklabels(), visible=False)
-        plt.setp(ax2.get_yaxis().get_offset_text(), visible=False)
+        ax1 = fig.add_subplotspec((2, 2), (0, 0), hidex=True)
+        ax2 = fig.add_subplotspec((2, 2), (1, 0), hidey=True, sharex=ax1)
+        ax3 = fig.add_subplotspec((2, 2), (0, 1), rowspan=2)
 
         # plot the posterior and data
-        ax1.set_title('current model')
-        ax1.plot_banded(x, mu, 2*np.sqrt(s2), label='posterior')
-        ax1.scatter(xdata, ydata, label='data')
-        ax1.axvline(xbest, label='recomm.')
-        ax1.legend(loc=3, bbox_to_anchor=(1, 0))
+        ax1.plot_banded(x, mu, 2*np.sqrt(s2))
+        ax1.scatter(xdata, ydata)
+        ax1.axvline(xbest)
+        ax1.axvline(xnext, color='g')
+        ax1.set_ylim(-6, 3)
+        ax1.set_title('current model (xbest and xnext)')
 
         # plot the acquisition function
-        ax2.set_title('current policy')
-        ax2.plot_banded(x, alpha, label='acquisition')
-        ax2.axvline(xnext, label='next query')
+        ax2.plot_banded(x, alpha)
+        ax2.axvline(xnext, color='g')
         ax2.set_xlim(*bounds)
-        ax2.legend(loc=3, bbox_to_anchor=(1, 0))
+        ax2.set_title('current policy (xnext)')
 
         # plot the latent function at recomended points
-        ax3.set_title('value of recommendation')
         ax3.axhline(f.get_f(f.xopt))
         ax3.plot(fbest)
+        ax3.set_ylim(0.4, 0.9)
+        ax3.set_title('value of recommendation')
 
         # draw
         fig.canvas.draw()
+        show()
 
         # add the next evaluation
         model.add_data(xnext, f(xnext))
--- pybo/demos/solve.py
+++ pybo/demos/solve.py
@@ -5,11 +5,29 @@ algorithm as well as the final model. The point `xbest[-1]` is the final
 recommendation, i.e. the expected maximizer.
 """
 
-from benchfunk.functions import Sinusoidal
+import numpy as np
+
+from ezplot import figure, show
+from benchfunk import Sinusoidal
 from pybo import solve_bayesopt
 
 
 if __name__ == '__main__':
-    # grab a test function and points at which to plot things
+    # grab a test function
     f = Sinusoidal()
+
+    # solve the model
     xbest, model = solve_bayesopt(f, f.bounds, niter=30, verbose=True)
+
+    # make some predictions
+    x = np.linspace(f.bounds[0, 0], f.bounds[0, 1], 500)
+    mu, s2 = model.predict(x[:, None])
+
+    # plot the final model
+    fig = figure()
+    axs = fig.gca()
+    axs.plot_banded(x, mu, 2*np.sqrt(s2))
+    axs.axvline(xbest[-1])
+    axs.scatter(model.data[0].ravel(), model.data[1])
+    fig.canvas.draw()
+    show()
