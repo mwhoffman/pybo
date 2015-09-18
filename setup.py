e7868cca80981937617f71526aac540b15d3044d
Matt Hoffman <mwh30@cam.ac.uk>
2015-09-18 14:19:03 +0100

    Version bump.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ Setup script for pybo.
 from setuptools import setup, find_packages
 
 setup(name='pybo',
-      version='0.0.1',
+      version='0.1',
       author='Matthew W. Hoffman',
       author_email='mwh30@cam.ac.uk',
       description='A Python package for modular Bayesian optimization',
